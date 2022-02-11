0cbc04a0b239131c7f307c5d15d280d93c50b681
Akhil15011997 <akhilsrinivas97@gmail.com>
2022-02-11 14:12:49 +0000

    Code changes in Runner class.

--- src/test/java/RunnerFiles/Runner.java
+++ src/test/java/RunnerFiles/Runner.java
@@ -8,15 +8,8 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features/Scripts.feature",glue= {"StepDef"},
-				 monochrome = true,plugin={"pretty","html:target/HTMLReports/report.html"}
-				 //,tags="{@Test}" 
-						 )
+				 monochrome = true,plugin={"pretty","html:target/HTMLReports/report.html"}  )
 
 public class Runner {
 
 }
-
-
-//plugin={"pretty","json:target/Report.json"}
-//plugin={"pretty","html:target/HTMLReports/report.html"}
-//plugin={"pretty","junit:target/Report.xml"}
